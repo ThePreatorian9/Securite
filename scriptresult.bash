0fad54d0dec764489bedbf69199d4cb6aa62cee4
ThePreatorian9 <122523486+ThePreatorian9@users.noreply.github.com>
2023-02-23 14:21:21 +0100

    Update scriptresult.bash

--- scriptresult.bash
+++ scriptresult.bash
@@ -1,6 +1,6 @@
 #!bin/bash
 
-#docker run aquasec/trivy image   python:3.10.8-alpine3.17 > resultatanalyse.txt && awk "/^Total/{print}" resultatanalyse.txt
+docker run aquasec/trivy image   python:3.10.8-alpine3.17 > resultatanalyse.txt && awk "/^Total/{print}" resultatanalyse.txt
 save=$(awk "/^Total/{print}" resultatanalyse.txt  | head -n1 | awk '{print $2;}')
 echo $save
 if [ $save -ne 0 ]; then
