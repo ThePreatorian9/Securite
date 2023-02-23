191612c3b3ca4284e3820eb5af11c0a9a27da7b8
ThePreatorian9 <122523486+ThePreatorian9@users.noreply.github.com>
2023-02-23 15:14:12 +0100

    Update scriptresult.bash

--- scriptresult.bash
+++ scriptresult.bash
@@ -3,9 +3,13 @@
 docker run aquasec/trivy image   python:3.10.8-alpine3.17 > resultatanalyse.txt && awk "/^Total/{print}" resultatanalyse.txt
 save=$(awk "/^Total/{print}" resultatanalyse.txt  | head -n1 | awk '{print $2;}')
 echo $save
-if [ $save -ne 0 ]; then
+if [ z "$save"]; then
         echo "In If erreur détecter "
 else
-        echo "In Else"
+         if [ $save -ne 0 ]; then
+         echo " error"
+
+         fi
 fi
+
 
