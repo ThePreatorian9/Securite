447232ff3ad7f23349a2198f66c3d738f4a76051
ThePreatorian9 <122523486+ThePreatorian9@users.noreply.github.com>
2023-02-23 22:24:21 +0100

    Update scriptresult.bash

--- scriptresult.bash
+++ scriptresult.bash
@@ -1,6 +1,6 @@
 #!bin/bash
 
-ocker run  -v /var/run/docker.sock:/var/run/docker.sock aquasec/trivy:0.18.3 image   $1 > resultatanalyse.txt && awk "/^Total/{print}" resultatanalyse.txt
+docker run  -v /var/run/docker.sock:/var/run/docker.sock aquasec/trivy:0.18.3 image   $1 > resultatanalyse.txt && awk "/^Total/{print}" resultatanalyse.txt
 save=$(awk "/^Total/{print}" resultatanalyse.txt  | head -n1 | awk '{print $2;}')
 echo $save
 if [ $save -ne 0 ]; then
